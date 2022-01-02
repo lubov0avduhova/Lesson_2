91363d962539f97f398a3ccd5f8708a6a5a67713
lubov0avduhova <loveavduhova@gmail.com>
2022-01-02 20:22:35 +0700

    ex 2 done

---
+++ ex.2/Program.cs
@@ -0,0 +1,6 @@
+﻿// 2. Написать метод подсчета количества цифр числа.
+Console.Write ("Введи число: ");
+
+string number = Console.ReadLine();
+
+Console.Write($"Количество символов: {number.Length}");
