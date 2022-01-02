c3530d1581fbd716b1d214f8ae7308d84f317e99
lubov0avduhova <loveavduhova@gmail.com>
2022-01-02 20:19:39 +0700

    ex 1 done

--- ex.1/Program.cs
+++ ex.1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//1. Написать метод, возвращающий минимальное из трёх чисел.
+
+Console.Write("Введи число а: ");
+int a = int.Parse (Console.ReadLine());
+Console.Write("Введи число b: ");
+int b = int.Parse (Console.ReadLine());
+Console.Write("Введи число c: ");
+int c = int.Parse (Console.ReadLine());
+int min = 0;
+
+if (a>b & a>c)
+{
+    min = a;
+   
+}
+else if (b > c)
+{
+    min = b;
+}
+else
+{
+    min = c;
+}
+Console.Write ($"Минимальное число: {min}");
